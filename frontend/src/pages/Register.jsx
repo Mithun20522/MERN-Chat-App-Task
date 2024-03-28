c1b99b5e617ad68bfcd3662d449aa08c8a560fad
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-29 00:16:22 +0530

    register page UI done

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-        
+    <div className='max-w-lg mx-auto p-3 mt-20'>
+        <h1 className='text-3xl text-center mb-5'>Register Here</h1>
+        <form className='flex flex-col gap-5'>
+            <input 
+            type="text" 
+            name='username' 
+            placeholder='john' 
+            className='border  placeholder:text-slate-500 p-3 rounded-md text-center' 
+            />
+            <input 
+            type="email" 
+            name='email' 
+            placeholder='john@example.com' 
+            className='border placeholder:text-slate-500 p-3 rounded-md text-center' 
+            />
+            <input 
+            type="password" 
+            name='password' 
+            placeholder='*********' 
+            className='border placeholder:text-slate-500 p-3 rounded-md text-center'  
+            />
+            <button className='p-3 text-white font-semibold bg-slate-600 hover:bg-slate-800 rounded-md'>Register</button>
+        </form>
     </div>
   )
 }
