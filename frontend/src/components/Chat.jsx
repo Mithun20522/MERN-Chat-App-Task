2eb0741817dcb4c8edaec4da707d27eade25579b
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-30 11:41:22 +0530

    Conversation rendroing done.

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Conversation from './Conversation'
 
+
 const Chat = () => {
+  const arr = [{message:"hello how are you?"}, {message:"Im fine at wysa"}, {message:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium facere praesentium mollitia quis officia vero cupiditate omnis quibusdam quia fuga quasi placeat dolorum vel ut, labore illum voluptatem sed beatae."}];
   return (
     <section>
-        <Conversation/>
+        <Conversation arr ={arr}/>
     </section>
   )
 }
